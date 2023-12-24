95ffa801035f32c03cbe827b4e316fa9ee3cc6c9
Markus Heck <markus.heck13396@gmx.de>
2023-12-24 07:25:07 +0100

    fix update entry

--- app/Http/Controllers/EntriesController.php
+++ app/Http/Controllers/EntriesController.php
@@ -50,8 +50,47 @@ class EntriesController extends Controller
 
     public function update($id, Request $request)
     {
-        $originalEntry = Entry::findOrFail($id);
-        $updatedData = $request->all();
+        $request['id'] = $id; // add id to request so it can be validated
+        // Validate the request
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|integer|exists:entries,id',
+            'category_id' => 'sometimes|integer|exists:categories,id',
+            'amount' => 'sometimes|numeric',
+            'recipient_sender' => 'sometimes|string|max:255',
+            'payment_method' => 'sometimes|in:cash,bank_transfer,not_payed',
+            'description' => 'sometimes|string',
+            'no_invoice' => 'sometimes|boolean',
+            'date' => 'sometimes|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+
+        try {
+            $originalEntry = Entry::findOrFail($id);
+        } catch (\Exception $e) {
+            // Check if the entry exists, including soft-deleted entries
+            $originalEntry = Entry::withTrashed()->where('id', $id)->first();
+
+            // Check if entry is not found or if it's soft-deleted
+            if (!$originalEntry) {
+                return response()->json(['error' => 'Entry not found'], 404);
+            } elseif ($originalEntry->trashed()) {
+                return response()->json(['error' => 'Entry is deleted'], 410); // 410 Gone status code
+            }
+        }
+
+        $updatedData = [
+            'category_id' => $request->input('category_id', $originalEntry->category_id),
+            'amount' => $request->input('amount', $originalEntry->amount),
+            'recipient_sender' => $request->input('recipient_sender', $originalEntry->recipient_sender),
+            'payment_method' => $request->input('payment_method', $originalEntry->payment_method),
+            'description' => $request->input('description', $originalEntry->description),
+            'no_invoice' => $request->input('no_invoice', $originalEntry->no_invoice),
+            'date' => $request->input('date', $originalEntry->date),
+        ];
 
         // Create a new entry with the updated data
         $newEntry = $originalEntry->replicateWithHistory($updatedData, Auth::id());
